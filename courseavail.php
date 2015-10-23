93a01daaf3ca132206a59af5452a7d1658335146
Kirk <kliserman@gmail.com>
2015-10-22 22:59:03 -0700

    cleaned up format
    cleaned up format of code (indentations, etc.)

--- courseavail.php
+++ courseavail.php
@@ -24,21 +24,21 @@ foreach ($schools as $school) {
     $subjects = $results->data;
 
     foreach($subjects as $subject) {
-	$subjectid = $subject[0];
+		$subjectid = $subject[0];
 
-	echo "$subjectid\n";
-	$args = array('subjectid' => $subjectid, 'term' => $term);
-	$results = $client->__soapCall('qCourses', $args);
-	$courses = $results->data;
+		echo "$subjectid\n";
+		$args = array('subjectid' => $subjectid, 'term' => $term);
+		$results = $client->__soapCall('qCourses', $args);
+		$courses = $results->data;
 
-	foreach ($courses as $course) {
-	    print_r($course);
-	    $courseid = $course[5];
+		foreach ($courses as $course) {
+	   		print_r($course);
+	    	$courseid = $course[5];
 
-	    $args = array('courseid' => $courseid, 'term' => $term);
-	    $results = $client->__soapCall('qCourse', $args);
-	    print_r($results);
-	}
+	    	$args = array('courseid' => $courseid, 'term' => $term);
+	    	$results = $client->__soapCall('qCourse', $args);
+	    	print_r($results);
+		}
     }
 }
 ?>
