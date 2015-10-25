c74edac8c8ab2ea1786396f1eb19e87d565db27f
Kirk <kliserman@gmail.com>
2015-10-24 23:18:34 -0700

    Wrote javascript validation

---
+++ student_form.php
@@ -0,0 +1,16 @@
+<?php
+//should handle student info here
+
+//need to double check valid student data here, as Prof Atkinson said, can't
+//trust anything sent from client side, even with JS validation on that side
+
+
+//if student not in waitlist already, add, and return something along the lines of the 
+//existing student_success.html
+
+//if already in waitlist, then return something along the lines of how we showed the
+//"your're already in the waitlist." or we could just ignore returning something different,
+//and just not re-add to the waitlist
+
+
+?>
--- student_form_check.js
+++ student_form_check.js
@@ -1,3 +1,36 @@
-function validateForm(){
+function validateForm(studentForm){
+	var firstName = document.getElementById("first_name").value;
+	var lastName = document.getElementById("last_name").value;
+	var ID = document.getElementById("ID_number").value;
+	var email = document.getElementById("email").value;
+	var reason = document.getElementById("reason").value;
+	var idCheck = /^(W|w)[0-9]{7}$/;
 
+	if(!firstName){
+		alert("Please enter your first name");
+		return false;
+	}
+	if(!lastName){
+		alert("Please enter your last name");
+		return false;
+	}
+	if(!ID){
+		alert("Please enter your student ID");
+		return false;
+	}
+	if(!email){
+		alert("Please enter your e-mail address");
+		return false;
+	}
+	if(!reason){
+		alert("Please enter your reason for needing the class.");
+		return false;
+	}
+	
+	if(!idCheck.test(ID)){
+		alert("Please enter a valid Student ID.");
+		return false;
+	}
+
+	return true;
 }
